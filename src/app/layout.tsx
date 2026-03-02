7056524629ef94da94c839c0a1ab1ed6ceced97f
korn-dev <kasikorntripforwork@gmail.com>
2026-03-03 00:13:47 +0700

    SETUP : shadcn, react query dev tool

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,9 @@ import type { Metadata } from "next";
 import { Geist } from "next/font/google";
 
 import { TRPCReactProvider } from "@/trpc/react";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { Toaster } from "@/components/ui/sonner";
+import { ThemeProvider } from "@/components/ui/theme-provider";
 
 export const metadata: Metadata = {
 	title: "Wisdom Education Platform",
@@ -20,9 +23,21 @@ export default function RootLayout({
 	children,
 }: Readonly<{ children: React.ReactNode }>) {
 	return (
-		<html className={`${geist.variable}`} lang="en">
+		<html className={`${geist.variable}`} lang="en" suppressHydrationWarning>
 			<body>
-				<TRPCReactProvider>{children}</TRPCReactProvider>
+				<TRPCReactProvider>
+					<ThemeProvider
+						attribute="class"
+						defaultTheme="system"
+						enableSystem
+						disableTransitionOnChange
+					>
+						<TooltipProvider>
+							{children}
+							<Toaster />
+						</TooltipProvider>
+					</ThemeProvider>
+				</TRPCReactProvider>
 			</body>
 		</html>
 	);
