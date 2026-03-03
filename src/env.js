8e76e5f60f09bc98879bca90ac4e85d89f58008d
korn-dev <kasikorntripforwork@gmail.com>
2026-03-03 11:33:09 +0700

    FIX : anon key to publishable key

--- src/env.js
+++ src/env.js
@@ -20,7 +20,7 @@ export const env = createEnv({
 	 */
 	client: {
 		NEXT_PUBLIC_SUPABASE_DB_URL: z.string().url(),
-		NEXT_PUBLIC_SUPABASE_ANON_KEY: z.string(),
+		NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY: z.string(),
 	},
 
 	/**
@@ -31,7 +31,8 @@ export const env = createEnv({
 		NODE_ENV: process.env.NODE_ENV,
 		SUPABASE_DB_URL: process.env.SUPABASE_DB_URL,
 		NEXT_PUBLIC_SUPABASE_DB_URL: process.env.NEXT_PUBLIC_SUPABASE_DB_URL,
-		NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+		NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY:
+			process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY,
 	},
 	/**
 	 * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
