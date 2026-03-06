1972c69bd9e32573f1709e65e12e8db3bd1dbbc1
korn-dev <kasikorntripforwork@gmail.com>
2026-03-06 16:21:32 +0700

    FIX : delete unused doc or rename supabase browser client

--- src/lib/supabase/client.ts
+++ src/lib/supabase/client.ts
@@ -1,7 +1,7 @@
 import { createBrowserClient } from '@supabase/ssr'
 import { env } from '@/env'
 
-export function createSupabaseClient() {
+export function createSupabaseBrowserClient() {
   return createBrowserClient(
     env.NEXT_PUBLIC_SUPABASE_URL,
     env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY
